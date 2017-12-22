e2d3c53e8e7f98e6451817d85bb638ce6cca2195
victorhong1988 <victorhong1988@gmail.com>
2017-12-22 15:29:48 -0500

    Add tap gesture and spawn portal scene

--- ARPortal/ViewController.swift
+++ ARPortal/ViewController.swift
@@ -23,6 +23,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         self.configuration.planeDetection = .horizontal
         self.sceneView.session.run(configuration)
         self.sceneView.delegate = self
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
         
     }
 
@@ -30,7 +32,38 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        
+        guard let sceneView = sender.view as? ARSCNView else {return}
+        let touchLocation = sender.location(in: sceneView)
+        let hitTestResult = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+        if !hitTestResult.isEmpty {
+            //were going to add room
+            self.addPortal(hitTestResult: hitTestResult.first!)
+        } else {
+            //
+        }
+        
+    }
 
+    func addPortal(hitTestResult: ARHitTestResult) {
+        
+        let portalScene = SCNScene(named: "Portal.scnassets/Portal.scn")
+        let portalNode = portalScene!.rootNode.childNode(withName: "Portal", recursively: false)!
+        let transform = hitTestResult.worldTransform
+        let planeXposition = transform.columns.3.x
+        let planeYposition = transform.columns.3.y
+        let planeZposition = transform.columns.3.z
+        portalNode.position = SCNVector3(planeXposition, planeYposition, planeZposition)
+        self.sceneView.scene.rootNode.addChildNode(portalNode)
+        self.addPlane(nodeName: "roof", portalNode: portalNode, imageName: "top")
+    }
+    
+    func addPlane(nodeName: String, portalNode: SCNNode, imageName: String) {
+        
+    }
+    
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         
         guard anchor is ARPlaneAnchor else {return}
